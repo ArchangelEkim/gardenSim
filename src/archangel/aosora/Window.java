83a5f27042dee50c2f977ba293023ffc26b30863
ArchangelEkim <michaelclayden00@gmail.com>
2020-12-27 16:34:25 +1100

    Initial Setup of 3D Garden Simulation Game while making a 3D game engine.

---
+++ src/archangel/aosora/Window.java
@@ -0,0 +1,15 @@
+package archangel.aosora;
+
+import org.lwjgl.Version;
+
+public class Window {
+
+	public Window() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+	}
+
+}
