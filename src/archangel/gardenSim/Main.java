fc2722ba6cac3dd3f8a680176032e62dba432557
ArchangelEkim <michaelclayden00@gmail.com>
2020-12-27 18:49:34 +1100

    Create a Window class in engine util package

---
+++ src/archangel/gardenSim/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package archangel.gardenSim;
+
+import archangel.aosora.util.Window;
+
+/**
+ * @author Michael Clayden
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Window window = Window.getInstance();
+		window.run();
+	}
+
+}
